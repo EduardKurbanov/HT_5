b464d88b35254126b5c7119053648d25ac9ced99
Eduard <48622382+EduardKurbanov@users.noreply.github.com>
2021-12-02 18:03:48 +0200

    Update 6.py

--- 6.py
+++ 6.py
@@ -7,24 +7,54 @@
 """
 
 
-def new_range(start=0, stop=0, step=1):
-    if start > 0 and stop == 0:
-        stop = start
-        start = 0
-    elif step == 0:
-        raise ValueError("range() arg 3 must not be zero")
-    elif start > 0 and stop > 0 and start > stop:
-        return
+def new_range(start=None, stop=None, step=None):
+    if (type(start).__name__ and type(stop).__name__ and type(step).__name__) != 'int':
+        if type(start).__name__ != 'int':
+            if type(start).__name__ == 'NoneType':
+                raise TypeError("range expected at least 1 argument, got 0")
+            else:
+                raise TypeError("'{0}' object cannot be interpreted as an integer".format(type(start).__name__ ))
+        elif type(stop).__name__ != 'int' and type(stop).__name__ != 'NoneType':
+            raise TypeError("'{0}' object cannot be interpreted as an integer".format(type(stop).__name__))
+        elif type(step).__name__ != 'int' and type(step).__name__ != 'NoneType':
+            raise TypeError("'{0}' object cannot be interpreted as an integer".format(type(step).__name__))
 
-    i = start
-    if start < stop and step != 0:
-        while i < stop:
+    new_start = int(start)
+    if stop is None:
+        new_stop = 0
+    else:
+        new_stop = int(stop)
+
+    if step is None:
+        new_step = 0
+    else:
+        new_step = int(step)
+
+    if new_start > 0 and stop is None and step is None:
+        new_stop = new_start
+        new_start = 0
+        new_step = 1
+    elif new_start < 0 and stop is None and step is None:
+        return
+    elif new_start <= 0 and new_stop > new_start and step is None:
+        new_step = 1
+    elif new_start > 0 and new_start > new_stop and (new_stop <= 0 or new_stop >= 0):
+        return
+    elif new_step == 0 or new_step < 0:
+        if new_step == 0:
+            raise ValueError("range() arg 3 must not be zero")
+        else:
+            return
+
+    i = new_start
+    if new_start < new_stop and new_step != 0:
+        while i < new_stop:
             yield i
-            i += step
-    elif start > stop:
-        while i > stop:
+            i += new_step
+    elif new_start > new_stop:
+        while i > new_stop:
             yield i
-            i += step
+            i += new_step
 
 
 for i in new_range(10):
